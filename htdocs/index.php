ef150c62e4f9a59a64d3862e7649931159d21194
Adrian Humphrey <humphar@bgsu.edu>
2021-09-12 16:35:15 -0400

    Assignment 01

---
+++ htdocs/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang = "en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Assignment 01 - Humphrey, Adrian</title>
+  </head>
+  <body>
+    <h1><center>Assignment 01 - Humphrey, Adrian</center></h1>
+      <!--Summary section-->
+      <div id= "Summary">
+      <h2>Summary</h2>
+      <p>My name is Adrian Humphrey, I am very much interested in computer science. </br>
+         I am a Software Engineering major at Bowling Green state University. As </br>
+         for interesting facts, I enjoy customizing helmets, whether as prop helmets or
+         bike helmets.</p>
+      </div>
+      <div>
+        <h2>Personal Information</h2>
+        <ul>
+          <li>Name: Adrian Humphrey</li>
+          <li>Address: 17 Rock Ridge Road, Denville, New Jersey</li>
+          <li>Phone: 973-567-8910</li>
+          <li>Email:<a href = "mailto:humphar@bgsu.edu">humphar@bgsu.edu</a></li>
+        </ul>
+      </div>
+      <!--Academic information section-->
+      <div>
+        <h2>Academic Information</h2>
+        <ul>
+          <li>Morris Knolls High School</li>
+          <ul>
+            <li>Within the top 100 New Jersey Highschools</li>
+            <li>I graduated within the top 15% of my class</li>
+            <li>Classes I enjoyed:</li>
+              <ul>
+                <li>Ap Computer Science A</li>
+                <li>Biology 1</li>
+                <li>Buisness Managment</li>
+              </ul>
+          </ul>
+          <li>Bowling Green State University</li>
+            <ul>
+            <li>Within the top 150 Public schools</li>
+            <li>I currently have a 4.0 GPA</li>
+            <li>Classes I have enjoyed:</li>
+              <ul>
+                <li>Intermidiate Programming</li>
+                <li>Computer Organizations</li>
+                <li>Intro to Ethics</li>
+              </ul>
+            </ul>
+        </ul>
+      </div>
+      <!--Employment Information section-->
+      <div>
+        <h2>Employment</h2>
+        <p>Past Employmet:</p>
+        <ol type = "I">
+          <li>Best Buy</li>
+          <li>Target</li>
+          <li>Code Ninjas</li>
+        </ol>
+        <p>For future Employment, I wish to program at Valve, Capital One, Microsoft </br>
+          or Verison.</p>
+      </div>
+  </body>
+</html>
